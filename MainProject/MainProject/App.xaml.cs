73460cd6bb0d4339ecea31a05799eaf0c7645987
Kaushik <nayanalakaushik@gmail.com>
2026-05-11 15:25:30 +0530

    Added blur effect and new image for starting page.

--- MainProject/MainProject/App.xaml.cs
+++ MainProject/MainProject/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace MainProject
 {
@@ -37,5 +38,44 @@ namespace MainProject
         {
             return Convert(value, targetType, parameter, culture);
         }
+    }
+
+    public class VisualBrushScaleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 6) return Transform.Identity;
+            double BEWt = (double)values[0];
+            double BEHt = (double)values[1];
+
+            double BIWt = (double)values[2];
+            double BIHt = (double)values[3];
+
+            double posx = (double)values[4];
+            double posy = (double)values[5];
+
+            double scalex = BIWt / BEWt;
+            double scaley = BIHt / BEHt;
+
+            double centerx = -(posx) / BEWt;
+            double centery = -(posy) / BEHt;
+
+
+            var group = new TransformGroup();
+            
+            // Apply Scale first to map the whole image to the label size
+            group.Children.Add(new ScaleTransform(scalex, scaley));
+
+            // Apply Translation to move the "window" to the correct segment
+            group.Children.Add(new TranslateTransform(centerx, centery));
+
+            return group;
+            //return tg;
+        }
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
